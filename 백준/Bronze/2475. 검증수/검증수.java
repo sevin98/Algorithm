8663573f32b76fe610fd2bdf4a49083c3b70af7e
sevin98 <117634128+sevin98@users.noreply.github.com>
2024-02-07 22:46:49 +0900

    [Bronze V] Title: 검증수, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			int temp = scanner.nextInt();
+			sum += temp * temp;
+		}
+
+		int res = sum % 10;
+		System.out.println(res);
+	}
+}
