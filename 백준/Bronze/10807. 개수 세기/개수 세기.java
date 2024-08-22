458f4e7483e8110ff916601318ab606947a1ea3b
박세빈 <117634128+sevin98@users.noreply.github.com>
2024-08-22 10:27:11 +0900

    [Bronze V] Title: 개수 세기, Time: 96 ms, Memory: 12972 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,26 +1,22 @@
-//package boj10807_개수세기;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int M = Integer.parseInt(br.readLine());
-		int cnt = 0;
-		for (int num : arr) {
-			if (num == M) {
-				cnt++;
-			}
-		}
-		System.out.println(cnt);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        sc.nextLine();
+        int[] arr =new int[N];
+
+        for(int i=0;i<N;i++){
+            arr[i]=sc.nextInt();
+        }
+        int number = sc.nextInt();
+        int cnt = 0;
+        for(int num : arr){
+            if(num==number){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
