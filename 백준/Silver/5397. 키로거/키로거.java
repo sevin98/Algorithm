6c8956a9bf963deffdf941b7565dc82d628a551c
박세빈 <117634128+sevin98@users.noreply.github.com>
2024-08-30 08:40:41 +0900

    [Silver II] Title: 키로거, Time: 556 ms, Memory: 320304 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,50 +1,92 @@
-//package boj5397_키로거;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine()); // tc 입력
 
-		for (int tc = 1; tc <= T; tc++) {
-			Stack<Character> s1 = new Stack<>();
-			Stack<Character> s2 = new Stack<>();
-			String str = br.readLine();
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < str.length(); i++) { //한 글자씩 입력 받으면서
-				switch (str.charAt(i)) {  // '<' 일떈 왼쪽껄 빼서 오른쪽에
-				case '<':
-					if (!s1.isEmpty()) {
-						s2.push(s1.pop());
-					}
-					break;
-				case '>':
-					if (!s2.isEmpty()) { // '>' 일떈 오른쪽껄 빼서 왼쪽에
-						s1.push(s2.pop());
-					}
-					break;
-				case '-':
-					if (!s1.isEmpty()) { // '-'일땐 왼쪽걸 지우고
-						s1.pop();
-					}
-					break;
-				default:
-					s1.push(str.charAt(i)); // 이외엔 다 push
-					break;
-				}
-			}
-			while(!s1.isEmpty()) { //s1에있는걸 역순으로 다 s2에넣고
-				s2.push(s1.pop());
-			}
-			while(!s2.isEmpty()) { //s2에있는걸 하나씩 빼면서 sb에 값저장
-			sb.append(s2.pop());
-			}
-			System.out.println(sb); //sb출력
-		}
+    static char[] data;
+    static int[] prev, next;
+    static int cursor, currIdx;
+    static final int MAX_LENGTH = 1_000_000;
 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
+            String line = br.readLine();
+
+            data = new char[MAX_LENGTH];
+            prev = new int[MAX_LENGTH];
+            next = new int[MAX_LENGTH];
+
+            cursor = 0;
+            currIdx = 1;
+
+            prev[0] = -1;
+            next[0] = -1;
+
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+
+                switch (c) {
+                    case '<':
+                        if (prev[cursor] != -1)
+                            cursor = prev[cursor];
+                        break;
+                    case '>':
+                        if (next[cursor] != -1)
+                            cursor = next[cursor];
+                        break;
+                    case '-':
+                        if (prev[cursor] != -1)
+                            delete();
+                        break;
+                    default:
+                        insert(c);
+                        break;
+                }
+            }
+            int index = next[0];
+            while (index != -1) {
+                sb.append(data[index]);
+                index = next[index];
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    static void insert(char ch) {
+        if (currIdx >= MAX_LENGTH)
+            return;
+
+
+        data[currIdx] = ch;
+        prev[currIdx] = cursor;
+        next[currIdx] = next[cursor];
+
+        if (next[cursor] != -1)
+            prev[next[cursor]] = currIdx;
+
+        next[cursor] = currIdx;
+        cursor = currIdx;
+        currIdx++;
+    }
+
+    static void delete() {
+        if (cursor == 0 || cursor >= currIdx)
+            return;
+
+
+        if (next[cursor] != -1)
+            prev[next[cursor]] = prev[cursor];
+
+        if (prev[cursor] != -1)
+            next[prev[cursor]] = next[cursor];
+
+        cursor = prev[cursor];
+    }
 }
