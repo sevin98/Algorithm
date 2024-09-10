c5ab4fd4b2b48a70debe72011651eb3cf5a81aec
박세빈 <117634128+sevin98@users.noreply.github.com>
2024-09-11 08:35:45 +0900

    [Silver III] Title: 회전하는 큐, Time: 64 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,58 +1,39 @@
-//package boj1021_회전하는큐;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		Deque<Integer> dq = new ArrayDeque<>();
-		int cnt = 0; // 이동횟수를 계산해줄cnt 변수
-		for (int i = 1; i <= N; i++) {
-			dq.offer(i); // 0번째인덱스부터 숫자 1번넣음
-		}
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			int index = getIndex(dq, num);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+        LinkedList<Integer> dq = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            dq.offer(i);
+        }
 
-			int size = dq.size();
-			int move;
-			if (index < size - index) {
-				move = index;
-			} else {
-				move = size - index;
-			}
-			for (int j = 0; j < move; j++) {
-				if (index < size - index) {
-					int first = dq.pollFirst();
-					dq.offerLast(first);
-					cnt++;
-				} else {
-					int last = dq.pollLast();
-					dq.offerFirst(last);
-					cnt++;
-				}
-			}
-			dq.pollFirst();
-		}
-		System.out.println(cnt);
-	}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int target = dq.indexOf(Integer.parseInt(st.nextToken()));
 
-	static int getIndex(Deque<Integer> dq, int target) {
-		int index = 0;
-		for (int num : dq) {
-			if (num == target)
-				return index;
-			index++;
-		}
-		return -1;
-	}
+            int half;
+            if (dq.size() % 2 == 0) half = (dq.size() / 2) - 1;
+            else half = (dq.size() / 2);
+
+            if (target > half) {
+                for (int j = 0; j < dq.size() - target; j++) {
+                    dq.offerFirst(dq.pollLast());
+                    cnt++;
+                }
+            } else {
+                for (int j = 0; j < target; j++) {
+                    dq.offerLast(dq.pollFirst());
+                    cnt++;
+                }
+            }
+            dq.pollFirst();
+        }
+        System.out.println(cnt);
+    }
 }
