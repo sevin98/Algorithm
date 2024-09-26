c2bc6f97dceca8bf8c10845e8d606e14ddcff3ba
박세빈 <117634128+sevin98@users.noreply.github.com>
2024-09-27 08:57:47 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 12964 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,93 +1,82 @@
-//package 백준10026_적록색약;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-	static int N;
-	static char[][] arr;
-	static boolean[][] check;
-	static int[] dr = {-1, 1, 0, 0};
-	static int[] dc = {0, 0, -1, 1};
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		
-		arr = new char[N][N];
-		check = new boolean[N][N];
-		
-		for(int r = 0; r< N; r++) {
-			String str = br.readLine();
-			for(int c = 0; c< N; c++) {
-				arr[r][c] = str.charAt(c);
-			}
-		}
-		
-		int generalcount = 0;
-		int honglvcount = 0;
-		
-		//general
-		for(int r = 0; r < N; r++) {
-			for(int c = 0 ; c< N; c++) {
-				if(!check[r][c]) {
-					bfs(r,c, arr[r][c]);
-					generalcount++;
-				}
-			}
-		}
-		
-		//check배열 초기화
-		for(int i = 0; i< N; i++) {
-			Arrays.fill(check[i], false);
-		}
-		
-		//배열값을 honglv값으로 바꾸고
-		//honglv
-		for(int r = 0 ; r< N; r++) {
-			for(int c = 0; c< N; c++) {
-				if(arr[r][c] == 'G') {//적록색약일때 초록색을찾으면 R로바꿈
-					arr[r][c] = 'R';
-				}
-			}
-		}
-		
-		//honglv bfs순회
-		for(int r = 0 ; r< N; r++) {
-			for(int c = 0 ;c<N; c++) {
-				if(!check[r][c]) {
-					bfs(r,c, arr[r][c]);
-					honglvcount++;
-				}
-			}
-		}
-		System.out.println(generalcount + " " + honglvcount);
-		
-	}
-	
-	static void bfs(int r, int c, char color) {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[]{r,c});
-		check[r][c] = true;
-		
-		while(!q.isEmpty()) {
-			int[] current = q.poll();
-			int curr = current[0];
-			int curc = current[1];			
-			
-			for(int i = 0; i<4; i++) {
-				int nr = curr + dr[i];
-				int nc = curc + dc[i];
-				
-				//배열범위 안에있고 색상이 같을때
-				if(nr >=0 && nr < N && nc >= 0 && nc < N && !check[nr][nc] && arr[nr][nc] == color) {
-					q.offer(new int[] {nr, nc});
-					check[nr][nc] = true;
-				}
-			}
-		}
-	}
+
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static char[][] arr;
+    static boolean[][] check;
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new char[N][N];
+        check = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = line.charAt(j);
+            }
+        }// arr
+
+        int areaCnt = 0;
+        int redAreaCnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!check[i][j]) {
+                    bfs(i, j, arr[i][j]);
+                    areaCnt++;
+                }
+            }
+        }
+
+        for(int i = 0 ; i < N; i++){
+            Arrays.fill(check[i], false);
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 'G') {
+                    arr[i][j] = 'R';
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!check[i][j]) {
+                    bfs(i, j, arr[i][j]);
+                    redAreaCnt++;
+                }
+            }
+        }
+        System.out.println(areaCnt + " " + redAreaCnt);
+    }
+
+    static void bfs(int row, int col, char color) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{row, col});
+        check[row][col] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int r = cur[0];
+            int c = cur[1];
+
+            for (int d = 0; d < 4; d++) {
+                int nr = dr[d] + r;
+                int nc = c + dc[d];
+
+                if (nr >= 0 && nc >= 0 && nr < N && nc < N && !check[nr][nc] && arr[nr][nc] == color) {
+                    q.offer(new int[]{nr, nc});
+                    check[nr][nc] = true;
+                }
+            }
+        }
+    }
 }
